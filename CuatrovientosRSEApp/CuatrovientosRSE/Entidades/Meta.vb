18be68263add3ec0df3cdaaa9f8938e92581918a
Asier <asielng2a@gmail.com>
2024-04-24 10:28:37 +0200

    primer dia

---
+++ CuatrovientosRSEApp/CuatrovientosRSE/Entidades/Meta.vb
@@ -0,0 +1,31 @@
+﻿Public Class Meta
+    Implements IEquatable(Of Meta)
+
+    Private Property NumeroODS As Byte
+    Private Property NumeroMeta As Byte
+    Private Property Descripcion As String
+
+    Public Overloads Function ToString()
+        Return $"{NumeroODS}.{NumeroMeta}: {Descripcion}"
+    End Function
+    Public Sub New(numeroODS As Byte, numeroMeta As Byte)
+        Me.NumeroODS = numeroODS
+        Me.NumeroMeta = numeroMeta
+        Me.Descripcion = ""
+    End Sub
+    Public Sub New(numeroODS As Byte, numeroMeta As Byte, descripcion As String)
+        Me.NumeroODS = numeroODS
+        Me.NumeroMeta = numeroMeta
+        Me.Descripcion = descripcion
+    End Sub
+    Public Overrides Function Equals(obj As Object) As Boolean
+        Return Equals(TryCast(obj, Meta))
+    End Function
+
+    Public Overloads Function Equals(other As Meta) As Boolean Implements IEquatable(Of Meta).Equals
+        Return other IsNot Nothing AndAlso
+               NumeroODS = other.NumeroODS AndAlso
+               NumeroMeta = other.NumeroMeta
+    End Function
+
+End Class
---
+++ CuatrovientosRSEApp/CuatrovientosRSE/Entidades/ODS.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Collections.ObjectModel
+
+Public Class ODS
+    Implements IEquatable(Of ODS)
+
+    Private Property NumeroODS As Byte
+    Private Property Nombre As String
+    Private Property Descripcion As String
+    Private Property _Metas As List(Of Meta)
+    Public ReadOnly Property Metas As ReadOnlyCollection(Of Meta)
+        Get
+            Return _Metas.AsReadOnly
+        End Get
+    End Property
+    Public Sub New(numeroODS As Byte)
+        Me.NumeroODS = numeroODS
+        Me.Nombre = ""
+        Me.Descripcion = ""
+        Me.Descripcion = ""
+    End Sub
+    Public Overrides Function Equals(obj As Object) As Boolean
+        Return Equals(TryCast(obj, ODS))
+    End Function
+
+    Public Overloads Function Equals(other As ODS) As Boolean Implements IEquatable(Of ODS).Equals
+        Return other IsNot Nothing AndAlso
+               NumeroODS = other.NumeroODS
+    End Function
+    Public Function ODS(simple As Boolean)
+        If simple Then
+            Return $"{NumeroODS}: {Nombre}"
+        Else
+            Dim metasEnMensaje As String = ""
+            For Each meta In _Metas
+                metasEnMensaje += $"{meta.ToString}\n"
+            Next
+            Return $"{NumeroODS}: {Nombre}\n{Descripcion}\n{metasEnMensaje}"
+        End If
+    End Function
+End Class
